2450f88e12aa057092e6ab161a744b9e0a0ab4c6
lakshaykaushik <lakshay.kaushik@microsoft.com>
2020-11-18 12:49:50 +0530

    Initial Commit
    Removing node_modules

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# PublishHTMLReport
-Azdo Extension- Publish Jmeter Reports to Azure Devops
+# Publish HTML Reports
+This extension can be used to publish Jmeter HTML reports as a seperate tab(Parallel to Summary tab) on Azure Devops. Right now full support of Jmeter report has been developed, however this extension can also be extended to publish other simple HTML documents or complex HTML reports as well. 
+
+
+## Usage:
+
+### 1. Install the below extension in your azure devops org:
+https://marketplace.visualstudio.com/items?itemName=LakshayKaushik.PublishHTMLReports&targetId=c2bac9a7-71cb-49a9-84a5-acfb8db48105&utm_source=vstsproduct&utm_medium=ExtHubManageList
+
+
+
+### 2. Run command line Jmeter with -o arg in your azure pipeline.
+For e.g. jmeter -n -t TestAPI.jmx -l LoadReports\results.jtl -e -o LoadReports
+This produces result files and folders having detailed jmeter report of the run.
+
+
+### 3. Now, use the extension in your azure devops pipeline to publish this report on Azdo.
+
+```
+- task: publishhtmlreport@1
+  inputs:
+    htmlType: 'Jmeter'
+    JmeterReportsPath: '$(Build.SourcesDirectory)/LoadReports'
+```
+
+This will make the Jmeter report compatible to be viewed and analysed within azure devops.
---
+++ publishhtmlreport/index.ts
@@ -0,0 +1,29 @@
+import tl = require('azure-pipelines-task-lib/task');
+
+async function run() {
+    try {
+        const inputString: string | undefined = tl.getInput('htmlType', true);
+        if (inputString == 'bad') {
+            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+            return;
+        }
+        const jmeterPath: string | undefined = tl.getInput('JmeterReportsPath', false);
+        process.chdir(jmeterPath!);
+        console.log('##vso[task.addattachment type=firstscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'dashboard-commons.js');
+        console.log('##vso[task.addattachment type=secondscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'dashboard.js');
+        console.log('##vso[task.addattachment type=thirdscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'jquery.tablesorter.min.js');
+        console.log('##vso[task.addattachment type=fourthscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'jquery.flot.stack.js');
+        console.log('##vso[task.addattachment type=fifthscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'hashtable.js');
+        console.log('##vso[task.addattachment type=sixthscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'jquery.numberformatter-1.2.3.min.js');
+        console.log('##vso[task.addattachment type=seventhscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'curvedLines.js');
+        console.log('##vso[task.addattachment type=ninthscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'graph.js');
+        console.log('##vso[task.addattachment type=tenthscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'jquery-ui.min.js');
+        console.log('##vso[task.addattachment type=eleventhscriptname;name=content;]' + jmeterPath! + '/content/js/' + 'jquery.cookie.js');
+
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
