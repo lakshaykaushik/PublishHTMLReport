ba7be7b56121ff9d8a18fff9748dd74c9c12e648
lakshaykaushik <lakshay.kaushik@microsoft.com>
2020-11-19 14:29:22 +0530

    Added some info to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
+# Remove existing jmeter container if present
 docker rm -f jmeter
 
+
+# Set jmeter version the extension should support
 VERSION='5.3'
 URL=https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-${VERSION}.tgz
 
@@ -25,13 +29,20 @@ cp -r LoadReports/sbadmin2-1.0.7 .
 cp -r LoadReports/content/css content
 
 rm -rf LoadReports
+# Remove some unsupported fonts by azdo
 rm -rf sbadmin2-1.0.7/bower_components/bootstrap/dist/fonts/
 rm -rf sbadmin2-1.0.7/bower_components/font-awesome/fonts
 
+
+# Run npm install
 npm install
 cd publishhtmlreport
 npm install
+# Rename unsupported file name
 mv node_modules/resolve/.github/workflows/node-4+.yml node_modules/resolve/.github/workflows/node-4.yml
+
+
+## After Running bootstrap.sh, the repo is ready to be built. Do the usual npm run build to generate the vsix after updating the version number
 
 
 
